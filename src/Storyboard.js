c9b5d1363e000cd6cca302fd818d2336e973e122
Keith Kurak <keith.kurak@gmail.com>
2021-10-15 19:12:44 -0400

    switched to simple stack design, added remaining dependencies

--- src/Storyboard.js
+++ src/Storyboard.js
@@ -1,37 +1,24 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Feather } from "@expo/vector-icons";
 import SettingsScreen from "./SettingsScreen";
 import FriendsScreen from "./FriendsScreen";
 
 const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
 
 export default function Storyboard() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen
+      <Stack.Navigator>
+        <Stack.Screen
           name="Friends"
           component={FriendsScreen}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <Feather name="users" size={size} color={color} />
-            ),
-          }}
         />
-        <Tab.Screen
+        <Stack.Screen
           name="Settings"
           component={SettingsScreen}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <Feather name="settings" size={size} color={color} />
-            ),
-          }}
         />
-      </Tab.Navigator>
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
