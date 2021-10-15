d3f055daa4edeaf58cbc82d8bf2103d8f305c15a
Keith Kurak <keith.kurak@gmail.com>
2021-10-15 19:45:02 -0400

    added push to settings screen

--- src/Storyboard.js
+++ src/Storyboard.js
@@ -1,9 +1,11 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Feather } from "@expo/vector-icons";
 import SettingsScreen from "./SettingsScreen";
 import FriendsScreen from "./FriendsScreen";
 import ChatScreen from "./ChatScreen";
+import { Pressable } from "react-native";
 
 const Stack = createNativeStackNavigator();
 
@@ -14,15 +16,23 @@ export default function Storyboard() {
         <Stack.Screen
           name="Friends"
           component={FriendsScreen}
+          options={({ navigation }) => ({
+            headerRight: () => (
+              <Pressable
+                style={
+                  ({ pressed }) => [
+                    { opacity: pressed ? 0.5 : 1.0 },
+                  ] /* touchable with opaciity */
+                }
+                onPress={() => navigation.navigate("Settings")}
+              >
+                <Feather name="settings" size={18} />
+              </Pressable>
+            ),
+          })}
         />
-        <Stack.Screen
-          name="Settings"
-          component={SettingsScreen}
-        />
-        <Stack.Screen
-          name="Chat"
-          component={ChatScreen}
-        />
+        <Stack.Screen name="Settings" component={SettingsScreen} />
+        <Stack.Screen name="Chat" component={ChatScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
