253d3a566f0c4b4f798f208076239c0c875f8ac8
Keith Kurak <keith.kurak@gmail.com>
2021-10-24 17:12:41 -0400

    added firebase init via non-committed file

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, View } from "react-native";
+import { initializeApp } from "firebase/app";
 import Storyboard from "./src/Storyboard";
 import { StoreProvider } from "./src/RootStore";
+import firebaseConfig from './firebaseConfig.json';
+
+initializeApp(firebaseConfig);
 
 export default function App() {
   return (
