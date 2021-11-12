44f9a27780142798fab299c41e033fd5e93dc98a
Keith Kurak <keith.kurak@gmail.com>
2021-11-11 23:46:15 -0500

    can at least run in electron

---
+++ electron-webpack.js
@@ -0,0 +1,7 @@
+const { withExpoAdapter } = require('@expo/electron-adapter');
+
+module.exports = withExpoAdapter({
+  projectRoot: __dirname,
+  whiteListedModules: [ "mobx-react" ]
+  // Provide any overrides for electron-webpack: https://github.com/electron-userland/electron-webpack/blob/master/docs/en/configuration.md
+});
