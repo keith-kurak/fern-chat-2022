59002c283db944a931fefab59b6ba46d41467205
Keith Kurak <keith.kurak@gmail.com>
2021-12-16 23:48:14 -0500

    added workaround to firebase connection

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, View } from "react-native";
+import { initializeFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
 import Storyboard from "./src/Storyboard";
 import { StoreProvider } from "./src/RootStore";
 import firebaseConfig from './firebaseConfig.json';
 
-initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+// needed this due to connection failures on Android
+initializeFirestore(app, { experimentalForceLongPolling: true });
 
 export default function App() {
   return (
