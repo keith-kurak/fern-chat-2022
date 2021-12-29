65f32fc317b835e9879c9f1060a2886faeea8dde
Keith Kurak <keith.kurak@gmail.com>
2021-12-29 13:26:33 -0500

    added styles

--- src/LoginScreen.js
+++ src/LoginScreen.js
@@ -2,6 +2,17 @@ import React, { useState } from "react";
 import { View, Button, TextInput /* use these to accept username/ password */ } from "react-native";
 import { useStore } from './RootStore';
 
+// use these on each text input to make it not look so bad
+const textInputStyle = {
+  width: 250,
+  fontSize: 16,
+  borderWidth: 1,
+  borderRadius: 5,
+  borderColor: "whitesmoke",
+  padding: 5,
+  margin: 3,
+};
+
 export default function LoginScreen() {
   const rootStore = useStore();
 
